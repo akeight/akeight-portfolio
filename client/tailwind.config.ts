796a7db6017f37ec9515ac14076622ad99cc1840
allykeightley <allysonkeightley@outlook.com>
2026-06-25 21:22:58 -0700

    style: change main font from Inter to Inter Tight

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['InterTight', 'system-ui', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
       },
       fontSize: {
